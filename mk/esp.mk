8aa14f3c0d0d993cec53839c5ac941550a7e1cac
Dmitry Ponyatov <dponyatov@gmail.com>
2025-04-12 23:08:15 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- mk/esp.mk
+++ mk/esp.mk
@@ -1,6 +1,7 @@
 LX106_SDK = xtensa-lx106-elf-gcc8_4_0-esp-$(LX106_VER)-linux-amd64
 LX106_GZ  = $(LX106_SDK).tar.gz
 
+GZ += $(ESP)/xtensa-lx106-elf/bin/xtensa-lx106-elf-gcc
 $(ESP)/xtensa-lx106-elf/bin/xtensa-lx106-elf-gcc: $(DISTR)/ESP/$(LX106_GZ)
 	mkdir -p $(ESP) ; cd $(ESP) ; tar zx < $< && touch $@
 $(DISTR)/ESP/$(LX106_GZ):
@@ -10,13 +11,13 @@ RTOS8266     = ESP8266_RTOS_SDK-v$(RTOS8266_VER)
 RTOS8266_GZ  = $(RTOS8266).zip
 RTOS8266_URL = https://github.com/espressif/ESP8266_RTOS_SDK/releases/download/v$(RTOS8266_VER)
 
+GZ += $(ESP)/ESP8266_RTOS_SDK/README.md
 $(ESP)/ESP8266_RTOS_SDK/README.md: $(DISTR)/ESP/$(RTOS8266_GZ)
 	unzip -d $(ESP) $< && touch $@
 $(DISTR)/ESP/$(RTOS8266_GZ):
-	$(CURL) $@ $(RTOS8266_URL)/$(RTOS8266_GZ)
+	mkdir -p $(dir $@) ; $(CURL) $@ $(RTOS8266_URL)/$(RTOS8266_GZ)
 
-.PHONY: esptool
-esptool: $(ESP)/esptool/esptool.py
+GZ += $(ESP)/esptool/esptool.py
 $(ESP)/esptool/esptool.py:
 	$(GITREF) https://github.com/espressif/esptool $(dir $@)
-	cd $(dir $<) ; python3 -m venv . ; bin/python3 setup.py install
+	cd $(dir $@) ; python3 -m venv . ; bin/python3 setup.py install
