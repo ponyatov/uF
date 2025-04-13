a5cf3c2ffc9f4d4db7c8c5903250f48edeba1aca
Dmitry Ponyatov <dponyatov@gmail.com>
2025-04-13 20:38:20 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- cmake/xtensa-lx106-elf.cmake
+++ cmake/xtensa-lx106-elf.cmake
@@ -6,18 +6,8 @@ set(CMAKE_EXECUTABLE_SUFFIX ".esp8266")
 
 include(any_toolchain)
 
-set(ESP /home/dponyatov/esp)
-
-set(CMAKE_C_COMPILER   ${ESP}/${TOOLCHAIN_PREFIX}/bin/${TOOLCHAIN_PREFIX}-gcc)
-set(CMAKE_ASM_COMPILER ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER ${ESP}/${TOOLCHAIN_PREFIX}/bin/${TOOLCHAIN_PREFIX}-g++)
-set(CMAKE_LINKER       ${CMAKE_C_COMPILER})
-set(CMAKE_OBJCOPY      ${ESP}/${TOOLCHAIN_PREFIX}/bin/${TOOLCHAIN_PREFIX}-objcopy)
-set(CMAKE_SIZE         ${ESP}/${TOOLCHAIN_PREFIX}/bin/${TOOLCHAIN_PREFIX}-size)
-
 add_compile_options(
     -ffunction-sections -fdata-sections
-    -DXTENSA -DESP8266
     $<$<COMPILE_LANGUAGE:CXX>:-nostdinc++>
     $<$<COMPILE_LANGUAGE:CXX>:-fno-rtti>
     $<$<COMPILE_LANGUAGE:CXX>:-fno-exceptions>
@@ -33,7 +23,7 @@ add_link_options(
     # -L${ESP}/${TOOLCHAIN_PREFIX}/${TOOLCHAIN_PREFIX}/lib
     # -T ${CMAKE_SOURCE_DIR}/hw/${HW}/${CPU_}x_FLASH.ld
     # --specs=nano.specs
-    # -Wl,--start-group -lc -lm -lnosys   -Wl,--end-group
-    # -Wl,--start-group -lstdc++ -lsupc++ -Wl,--end-group
-    # -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--gc-sections
+    -Wl,--start-group -lc -lm -lnosys   -Wl,--end-group
+    -Wl,--start-group -lstdc++ -lsupc++ -Wl,--end-group
+    -Wl,-Map=${CMAKE_PROJECT_NAME}.map -Wl,--gc-sections
 )
