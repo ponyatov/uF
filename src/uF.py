839bc68c5a6e1b5fd4b2dfd312ad8338ee43c23a
Dmitry Ponyatov <dponyatov@gmail.com>
2025-04-13 23:15:44 +0400

    Signed-off-by: Dmitry Ponyatov <dponyatov@gmail.com>

--- src/uF.py
+++ src/uF.py
@@ -2,6 +2,9 @@
 
 import sys
 
+## data stack
+D = []
+
 ## print tracing log
 log = True
 
@@ -11,18 +14,21 @@ def bye():
         print('bye')
     sys.exit(0)
 
+
+import ply.lex as lex
+import ply.yacc as yacc
+
+
 import readline
 
 ## Read-Eval-Print-Loop
 def REPL():
     while True:
+        print(D)
         try: cmd = input('> ')
-        except EOFError: bye()
-
-        print(f'cmd: [{cmd}]\n')
+        except EOFError: break
 
 
 if __name__ == '__main__':
-    for src in sys.argv[1:]:
-
+    for src in sys.argv[1:]: print(src)
     REPL()
